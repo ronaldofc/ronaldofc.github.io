04959ffb6ba1dfa198d7e71ebe7f3edcb3bd0c63
ronaldofc <rfcorrea@byui.edu>
2020-02-18 06:48:57 -0700

    update weather-images

--- weather/js/lazi-loading-images.js
+++ weather/js/lazi-loading-images.js
@@ -9,39 +9,3 @@ const loadImages = (image) => {
 imagesToLoad.forEach((img) => {
   loadImages(img);
 });
-
-
-/*
-//grab all sorces with atribute data-src
-let imagesToLoad = document.querySelectorAll('source[data-src]');
-
-const loadImages = (image) => {
-        //make the image load before displayed
-        image.setAttribute('srcset', image.getAttribute('data-src'));
-        //after it displays
-        image.parentElement.lastElementChild.onload = () => {
-            image.removeAttribute('data-src');
-            image.parentElement.lastElementChild.className = 'clear';
-        };
-};
-
-//call load images for each image
-if('IntersectionObserver' in window) {
-    const observer = new IntersectionObserver((items, observer) => {
-      items.forEach((item) => {
-        if(item.isIntersecting) {
-          loadImages(item.target);
-          observer.unobserve(item.target);
-        }
-      });
-    });
-    imagesToLoad.forEach((source) => {
-      observer.observe(source);
-    });
-  } else {
-    imagesToLoad.forEach((source) => {
-      loadImages(source);
-    });
-  }
-
-  */
