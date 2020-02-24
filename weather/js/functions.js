04f964aa5b611831885d2c22d8a829939d38744f
ronaldofc <rfcorrea@byui.edu>
2020-02-24 06:21:57 -0700

    update

--- weather/js/functions.js
+++ weather/js/functions.js
@@ -1,5 +1,89 @@
-'use strict';
-
 // A collections of functions for the weather site
+//------------------------------------------------
 
-console.log('My javascript is being read.');
+'use strict';              
+
+console.log('My javascript is being read.');        //Test if this file is running
+
+
+
+/* GLOBAL VARIABLES
+*********************************************************************************/
+var currCond = "RAIN";          // Current condition of the weather
+var actualHour = 6;             // Time Indicator
+var speed = 10;
+var temp = 45;
+
+
+
+/* FUNCTIONS - Index:
+*********************************************************************************
+F000 - function()               - DOMContentLoaded
+F001 - buildModDate()           - Build the modified Data function 
+F002 - function actualDate()    - Build the actual date Customized
+F003 - getActualYear()          - To Get the actual Year
+F004 - hamMenu()                - To set the Hamburguer Menu for Small Screen
+F005 - updateFeelsLikeTemp()    - To Update feels Like Temperature
+F006 - buildWC()                - To Calculate the Feels Like Temperature
+
+***/
+
+
+
+//F000 - DOMContentLoaded
+document.addEventListener("DOMContentloaded", function () {
+    
+    buildModDate();                  // Call the modified Data 
+    hamMenu();                       // Call Hamburguer Menu 
+    updateFeelsLikeTemp();           // Call Update feels Like Temperature
+    timeIndicator(actualHour);       // Call Time Indicator
+    backGrdImage(currCond);          // Call Backgound Image
+
+});
+
+
+//F001 Build the modified Data function
+function buildModDate(){
+    document.getElementById("lastModif").innerHTML = document.lastModified;    
+};
+
+
+//F002 Build the actual date - Customized
+function actualDate(){
+    var now = new Date();
+    dayName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    monName = ["january", "Febuary", "March", "April", "Maio", "June", "July", "August", "September", "November","December"];
+    dateTest = dayName[now.getDay() ] + ", " + now.getDate() + " " + monName [now.getMonth() ] + " " + now.getFullYear ();
+    document.getElementById("test").innerHTML = dateTest;
+};
+
+
+//F003 To Get the actual Year
+function getActualYear(){
+    var today = new Date();
+    var year = today.getFullYear();
+    document.getElementById("footerYear").innerHTML = year;
+};
+
+
+//F004 To Set the hamburguer Menu for Small Screen
+function hamMenu(){
+    const hambutton = document.querySelector(".ham");
+    hambutton.addEventListener("click", hambutton);
+};
+
+//F005 To Update feels Like Temperature
+function updateFeelsLikeTemp(){
+    let feelTemp = document.getElementById('feelTemp');
+    feelTemp.innerHTML=buildWC(speed, temp);
+};
+
+
+//F006 To Calculate the Feels Like Temperature
+function buildWC(speed, temp) {
+    let feelsLike = (speed * temp );   //TODO -  create correct formula
+    return  feelsLike;
+};
+
+
+
