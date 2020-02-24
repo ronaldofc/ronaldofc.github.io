5b8da16cec86c7c31d60d49e493bd0b7b787c039
ronaldofc <rfcorrea@byui.edu>
2020-02-24 07:17:58 -0700

    update

--- weather/js/functions.js
+++ weather/js/functions.js
@@ -81,8 +81,11 @@ function updateFeelsLikeTemp(){
 
 //F006 To Calculate the Feels Like Temperature
 function buildWC(speed, temp) {
-    let feelsLike = (speed * temp );   //TODO -  create correct formula
-    return  feelsLike;
+    let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);  // Compute the windchill
+    console.log(wc);                // Verification console output
+    wc = Math.floor(wc);            // Round the answer down to integer
+    wc = (wc > temp)?temp:wc;       // If chill is greater than temp, return the temp
+    return  wc;
 };
 
 
